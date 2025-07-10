1e34aaf0fcfa0f878346e8fa5bbe0d9128e68f52
Dharshclin24 <shavonfdclinton@gmail.com>
2025-07-10 14:47:57 -0700

    changed leap year output

--- 03-modules-and-files/leapYear.js
+++ 03-modules-and-files/leapYear.js
@@ -1,8 +1,7 @@
 import moment from "moment";
 
-
-// * 
-// YOUR TASK: 
+// *
+// YOUR TASK:
 // Create a program that checks to see if the current year is a leap year.
 // Use the Moment module: https://www.npmjs.com/package/moment and read its documentation to find out how to determine whether a year is a leap year.
 
@@ -12,30 +11,18 @@ import moment from "moment";
 //     - "2024 is a leap year!"
 //     - "1979 is not a leap year!"
 // */
-const trueLYear = moment([2000]).isLeapYear() // true
+const trueLYear = moment([2000]).isLeapYear(); // true
 const falseLYear = moment([2001]).isLeapYear() // false
 console.log(trueLYear);
-console.log(falseLYear);
+//console.log(falseLYear);
 const userInputedYear = process.argv[2];
-console.log(userInputedYear);
+console.log(
+  ` ${userInputedYear} you entered is a ${
+    trueLYear ? "leap year" : "not a leap year"
+  }||${moment([userInputedYear]).isLeapYear()}.`
+);
 
-
-
-
-
-
-
-      
-       
-
-    
-
-
-
-
-
-
-// YOUR TASK: 
+// YOUR TASK:
 // Create a program that checks to see if the current year is a leap year.
 // Use the Moment module: https://www.npmjs.com/package/moment and read its documentation to find out how to determine whether a year is a leap year.
 
@@ -44,4 +31,3 @@ console.log(userInputedYear);
 // - Your program should output a console.log() message that says whether the inputted year is a leap year, such as...
 //     - "2024 is a leap year!"
 //     - "1979 is not a leap year!"
-
